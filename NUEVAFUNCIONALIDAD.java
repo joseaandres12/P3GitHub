512f033c8d505d55aeef73bc652813d32ae99c2e
joseaandres12 <joseandresmolinamejia1@gmail.com>
2024-03-11 12:21:02 +0100

    ejercicio6
    Este es el ejerciciio6

---
+++ NUEVAFUNCIONALIDAD.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.nuevafuncionalidad;
+
+/**
+ *
+ * @author Alumno
+ */
+public class NUEVAFUNCIONALIDAD {
+
+    public static void main(String[] args) {
+        System.out.println("eJERCICIO6");
+    }
+}
