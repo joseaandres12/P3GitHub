e7228136a849fa6c2628b3f7549e1547d41a6205
mikaditos <160121560+mikaditos@users.noreply.github.com>
2024-03-11 12:36:36 +0100

    archivo colaborador

---
+++ ARchivo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author Alumno
+ */
+public class ARchivo {
+
+    //Esto es para que muestre por pantalla la frase
+    public static void main(String[] args) {
+        System.out.println("Esto es de sergio");
+    }
+}
